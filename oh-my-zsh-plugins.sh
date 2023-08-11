d39535e9b72354573abcf144c765f8293a1129d0
Murtadha Marzouq <mmarzou0@gmail.com>
2023-08-11 06:05:22 -0400

    base16 tool

--- oh-my-zsh-plugins.sh
+++ oh-my-zsh-plugins.sh
@@ -34,7 +34,7 @@ function install_zsh_plugins(){
     DIR="${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
     git clone "${URL}" "${DIR}"
     # Install base16-shell
-    URL="https://github.com/chriskempson/base16-shell.git" 
+    URL="https://github.com/chriskempson/base16-shell.git"
     DIR="${HOME}/.oh-my-zsh/custom/plugins/base16-shell"
     git clone "${URL}" "${DIR}"
     # Enable plugins
@@ -51,5 +51,14 @@ install_zsh_plugins
 echo "Plugins installed!"
 
 
+omz plugin enable git zsh-syntax-highlighting zsh-navigation-tools base16-shell zsh-autosuggestions
+# ------------------------------------------------------------------------------
+# UPDATE ZSH SETTINGS
+source ~/.zshrc
+# ------------------------------------------------------------------------------
 
+# Setting the Base16 Theme
+base16_pop
+# ------------------------------------------------------------------------------
+1
 # ------------------------------------------------------------------------------
