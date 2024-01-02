d9470235145958496fd5cb0501c8b1a74616202b
Murtadha Marzouq <mmarzou0@gmail.com>
2024-01-01 20:19:55 -0500

    CHROMA TERM ADDED

--- oh-my-zsh-plugins.sh
+++ oh-my-zsh-plugins.sh
@@ -45,19 +45,25 @@ function install_zsh_plugins(){
 echo "Installing plugins..."
 install_zsh_plugins
 echo "Plugins installed!"
-
-
 # UPDATE ZSH SETTINGS
 source ~/.zshrc
 # ------------------------------------------------------------------------------
 omz plugin enable git zsh-syntax-highlighting zsh-navigation-tools base16-shell zsh-autosuggestions
 # ------------------------------------------------------------------------------
-
 # Setting the Base16 Theme
-source ~/.oh-my-zsh/custom/base16-shell/scripts/base16-pop.sh
+#source ~/.oh-my-zsh/custom/base16-shell/scripts/base16-pop.sh
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
 ### SETUP USEFUL ALIASES
 # ------------------------------------------------------------------------------
 
+# ------------------------------------------------------------------------------
+### SETUP CHROMA TERMINAL (Unix & Linux)
+# ------------------------------------------------------------------------------
+echo "Installing chroma terminal...ensure you are on a Unix or Linux system or WSL (Windows Subsystem for Linux)" 
+pip3 install chromaterm
+curl -L https://raw.githubusercontent.com/MurtadhaM/Infrastructure/main/Administration/Colorize%20CLI/chromaterm.yml -o ~/.chromaterm.yml
+TOOLS="ssh telnet minicom ifconfig ip netstat ping traceroute lsof sudo"
+echo $TOOLS | while read -d ' ' tool; do alias $tool="ct  $tool";done
+# ------------------------------------------------------------------------------
