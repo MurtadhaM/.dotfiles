e5b80ed91436dcd6a881102a200838e2f99fa1f9
Murtadha Marzouq <mmarzou0@gmail.com>
2024-02-23 04:42:37 -0500

    TMUX

--- tmux.sh
+++ tmux.sh
@@ -10,8 +10,14 @@ git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 # Set the default theme
 tmux set -g @themepack 'powerline/block/cyan'
 # Add the following line to your ~/.tmux.conf to use the theme
-echo tmux source-file ~/.tmux-themepack/powerline/block/red.tmuxtheme >> ~/.tmux.conf
+echo source-file ~/.tmux-themepack/powerline/double/orange.tmuxtheme > ~/.tmux.conf                                              
+# enable scroll
+echo set -g mouse on >> ~/.tmux.conf
 source ~/.zshrc
+
+omz plugin enable tmux tmuxinator
+
+
 
 }
 
