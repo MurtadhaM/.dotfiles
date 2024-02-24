ef3659f2eddbd9199ddaf58ef45f409164453df0
Murtadha Marzouq <mmarzou0@gmail.com>
2024-02-24 18:09:15 -0500

    PLUGINS

--- oh-my-zsh-plugins.sh
+++ oh-my-zsh-plugins.sh
@@ -35,25 +35,6 @@ function install_zsh_plugins(){
     echo "Updating plugins section..."
 }
 
-# ------------------------------------------------------------------------------
-
-echo "Installing plugins..."
-install_zsh_plugins
-echo "Plugins installed!"
-# UPDATE ZSH SETTINGS
-source ~/.zshrc
-# ------------------------------------------------------------------------------
-omz plugin enable git zsh-syntax-highlighting zsh-navigation-tools base16-shell zsh-autosuggestions z fzf tmux 
-
-# ------------------------------------------------------------------------------
-### SETUP CHROMA TERMINAL (Unix & Linux)
-# ------------------------------------------------------------------------------
-echo "Installing chroma terminal...ensure you are on a Unix or Linux system or WSL (Windows Subsystem for Linux)" 
-pip3 install chromaterm
-curl -L https://raw.githubusercontent.com/MurtadhaM/Infrastructure/main/Administration/Colorize%20CLI/chromaterm.yml -o ~/.chromaterm.yml
-#TOOLS="ssh telnet minicom ifconfig ip netstat ping traceroute lsof sudo"
-#echo $TOOLS | while read -d ' ' tool; do alias $tool="ct  $tool";done
-# ------------------------------------------------------------------------------
 
 
 #Base16 Theme
@@ -76,16 +57,32 @@ echo 'source ~/.oh-my-zsh/custom/plugins/base16-shell/scripts/base16-pop.sh' >> 
 
 
 
-
-
-
-
-
-
-
 function main(){
+# INSTALL ZSH PLUGINS
+install_zsh_plugins
+# ------------------------------------------------------------------------------
+# SETTINGS
+# ------------------------------------------------------------------------------
+# Path to oh-my-zsh installation
+# ZSH Terminal title
+DISABLE_AUTO_TITLE=true
+
 #------------------------------------------------------------------------------
+# Install oh-my-zsh
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# ------------------------------------------------------------------------------
+# Base16 Shell
+# ------------------------------------------------------------------------------
+enable_base16
+# ------------------------------------------------------------------------------
 # Change the theme and enable base16 theme
 base16_material-vivid
-enable_base16
+# ------------------------------------------------------------------------------
+# Enable plugins
+omz plugin enable tmux-cssh tmuxinator git zsh-syntax-highlighting zsh-navigation-tools base16-shell zsh-autosuggestions z fzf tmux tmux-cssh tmuxinator zsh-interactive-cd fzf git aliases themes
 }
+
+
+
+
