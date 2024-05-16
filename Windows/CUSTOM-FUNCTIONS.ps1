7c8e3e7bf2c4b66e3546860ec74cff2c155f9736
Murtadha Marzouq <mmarzou0@gmail.com>
2024-05-16 11:37:56 -0400

    WIndows

--- Windows/CUSTOM-FUNCTIONS.ps1
+++
@@ -1,4 +0,0 @@
-<# ALIASES & FUNCTIONS #>
-
-function ls(){dir.exe -lha @1}
-function l(){dir.exe -lha @1}
--- ZshrcCustomConfig.sh
+++
@@ -1,8 +0,0 @@
-cat <<EOF>> ~/.zshrc
-# ------------------------------------------------------------------------------
-export EDITOR=nano
-alias publicIP='curl -s https://api.ipify.org'
-eval "$(register-python-argcomplete az)"
-# TO FIND AN REMOVE ALL .DS_STORE FILES
-alias check_ip="curl http://icanhazip.com/"
-
